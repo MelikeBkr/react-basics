e1ac4edeb13619290bbe6a2b195aee72337f9bde
MelikeBkr <melikeyaman.bakir@gmail.com>
2022-08-09 16:37:46 +0100

    state is defined by using useState hook. It is set with the event triggerring

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 
-const Person = (props) => {
-  return (
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: {props.lastName}</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-  )
-}
 const App = ()=>{
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <Person name={'Salih'} lastName={'Bakir'} age={36}/>
-      <Person name={'Melike'} lastName={'Bakir'} age={31}/>
+      <button onClick={() => {setCounter((prevCount)=> prevCount-1)}}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => {setCounter((prevCount)=> prevCount+1)}}>+</button>
     </div>
   );
 }
